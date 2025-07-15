81fe98271e7ea2a796c6398f7c79ced29b6b0ec3
rMaxBarros <rmaxbarros@gmail.com>
2025-07-14 23:46:01 -0300

    creating and rendering header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Cabecalho } from './components/cabecalho/cabecalho';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Cabecalho],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
