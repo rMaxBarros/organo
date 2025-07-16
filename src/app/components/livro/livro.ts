2fa4797a0d2f2d7f3dfe02a12ca03a706f6931ee
rMaxBarros <rmaxbarros@gmail.com>
2025-07-15 22:15:10 -0300

    add favorite button to book card

--- src/app/components/livro/livro.ts
+++ src/app/components/livro/livro.ts
@@ -7,10 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './livro.css'
 })
 export class Livro {
+
+  alternarFavorito() {
+    this.livro.favorito = !this.livro.favorito;
+  }
   
   livro = {
     titulo: "Uma vida Pequena",
     autoria: "Hanya Yanagihara",
+    favorito: false,
     imagem: "https://m.media-amazon.com/images/I/810ru7lFZcS._UF894,1000_QL80_.jpg"
   }
 }
